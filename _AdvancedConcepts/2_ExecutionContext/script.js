aa522d0fd6f3ed07259a7e0be1c3d6e0cc0c1839
Ali <ha5403905@gmail.com>
2025-06-26 23:07:16 +0500

    functional execution context code added.

--- _AdvancedConcepts/2_ExecutionContext/script.js
+++ _AdvancedConcepts/2_ExecutionContext/script.js
@@ -34,3 +34,47 @@ function sayName() {
   return findName();
 }
 sayName();
+
+// Function invocation
+/*
+// this, arguments
+"this" => is the object that the function is a property of, gives methods access to their object, execute same code for multiple objects
+"arguments" => on each execution context, we get arguments object which contains the arguments passed to a function
+Variable Environment = variables created inside each execution context are stored in variable environment.
+*/
+// Function Declarations are fully Hoisted but Function Expressions don't. 
+
+// Function Declaration
+function pakistan(){
+  console.log("Hot");
+}
+
+// Function Expression
+var canada = () => {
+  console.log("Cold");
+}
+
+// Function Invocation/Call/Execution
+canada();
+pakistan();
+
+
+function marry(...args){
+  console.log(args);
+  console.log(arguments);
+  console.log(Array.from(arguments));
+  return `${args[0]} is now married to ${args[1]}`;
+
+}
+marry('moosa','rubab');
+
+// Variable Environment
+function two(){
+  var isValid;
+}
+function one(){
+  var isValid = true;
+  two();
+}
+var isValid = false;
+one();
