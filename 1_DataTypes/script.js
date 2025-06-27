67e09ab0b45f3dd2fceb45b82b1cda393157eac4
Ali <ha5403905@gmail.com>
2025-06-27 16:47:09 +0500

    small changes

--- 1_DataTypes/script.js
+++ 1_DataTypes/script.js
@@ -8,31 +8,40 @@ name = "Jane"; // Allowed
 age = 31; // Allowed
 // pi = 3.14159; // Not allowed, will throw an error
 
-// Data Types
+// Data-Types
 /*
  * Primitive: Number (int, float, exponential), BigInt, Boolean, String, Null, Undefined, Symbol
  * Non-Primitive (Object/Reference): Array[], Function(), Object{}
 */
 
-// Primitives
-let tint = 1;  // Integer
-let tfloat = 1.5;  // Float
-let texponential = 1e2;  // Exponential
-let texponential1 = 1e-2;  // Exponential
-let tbigint = BigInt(1212112324324798324234838324);  // BigInt
-let tboolean = true;  // Boolean
+
+// Primitive Types
+// Integer
+let tint = 1;  
+// Float
+let tfloat = 1.5;  
+// Exponential
+let texponential = 1e2;  
+// BigInt
+let tbigint = BigInt(1212112324324798324234838324);  
+// Boolean
+let tboolean = true;  
+// String
 let tstring = "Hello World";  // String (double quotes)
 let tstring1 = 'Hello World';  // String (single quotes)
 let tstring2 = `Hello World`;  // String (template literals)
-let tnull = null;  // Null
-let tundefined;  // Undefined
-let tsymbol = Symbol("Hello");  // Symbol
+// Null
+let tnull = null;  
+// Undefined
+let tundefined;  
+// Symbol
+let tsymbol = Symbol("Hello");  
+ 
 
-// Reference Types
+// Non-Primitive Types
 // Array
 let tarray = [1, 2, 3, 4, 5];
 // Function
-// Internally, a function is a special kind of object with callable behavior.
 let tfunction = () => { console.log("Hello from function"); };
 // Object
 let tobject = { name: "John", age: 30 };  
