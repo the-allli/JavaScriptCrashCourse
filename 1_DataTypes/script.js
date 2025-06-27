55011d24635ba0d642376974dc8b33ff801b43ee
Ali <ha5403905@gmail.com>
2025-06-27 21:44:59 +0500

    some changes

--- 1_DataTypes/script.js
+++ 1_DataTypes/script.js
@@ -1,7 +1,7 @@
 // Declaring & Initializing variables using var, let, and const
 var name = "John";
 let age = 30;
-const pi = 3.14; 
+const pi = 3.14;
 
 // Reassigning variables
 name = "Jane"; // Allowed
@@ -12,36 +12,37 @@ age = 31; // Allowed
 /*
  * Primitive: Number (int, float, exponential), BigInt, Boolean, String, Null, Undefined, Symbol
  * Non-Primitive (Object/Reference): Array[], Function(), Object{}
-*/
-
+ */
 
 // Primitive Types
 // Integer
-let tint = 1;  
+let tint = 1;
 // Float
-let tfloat = 1.5;  
+let tfloat = 1.5;
 // Exponential
-let texponential = 1e2;  
+let texponential = 1e2;
+let texponential1 = 1e-2;
 // BigInt
-let tbigint = BigInt(1212112324324798324234838324);  
+let tbigint = BigInt(1212112324324798324234838324);
 // Boolean
-let tboolean = true;  
+let tboolean = true;
 // String
-let tstring = "Hello World";  // String (double quotes)
-let tstring1 = 'Hello World';  // String (single quotes)
-let tstring2 = `Hello World`;  // String (template literals)
+let tstring = "Hello World"; // String (double quotes)
+let tstring1 = "Hello World"; // String (single quotes)
+let tstring2 = `Hello World`; // String (template literals)
 // Null
-let tnull = null;  
+let tnull = null;
 // Undefined
-let tundefined;  
+let tundefined = undefined;
 // Symbol
-let tsymbol = Symbol("Hello");  
- 
+let tsymbol = Symbol("Hello");
 
 // Non-Primitive Types
 // Array
 let tarray = [1, 2, 3, 4, 5];
 // Function
-let tfunction = () => { console.log("Hello from function"); };
+let tfunction = () => {
+  console.log("Hello from function");
+};
 // Object
-let tobject = { name: "John", age: 30 };  
+let tobject = { name: "John", age: 30 };
