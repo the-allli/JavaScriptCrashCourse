0bf748bb5d5380523ef50d3bd13188101b63ffe3
Ali <ha5403905@gmail.com>
2025-06-30 16:03:52 +0500

    small changes in the comments

--- _AdvancedConcepts/1_JSEngine/script.js
+++ _AdvancedConcepts/1_JSEngine/script.js
@@ -9,10 +9,12 @@ console.log(jsEngine("var a = 5"));
 JS-Engine
 {
   Parser => Lexical-Analysis(tokens)
+
   AST => Abstract-Syntax-Tree
+  
   Call-Stack[] => place to keep track of where we are in the code
   // each entry on the call-stack is called Frame
-  // it operates on the LIFO principal
+  // operates on the LIFO principal
   // Stack-Trace => snapshot of call-stack
   // Stack-Overflow => when call-stack size exceeded
   // Recursion is the common way to do stack-overflow
@@ -20,49 +22,41 @@ JS-Engine
   // arrays, functions, objects and complex data-structures are stored
   // Garbage-Collection => Mark-Sweep 
   // Memory-Leak => when memory-heap size exceeded
-  // global variables, event listeners, setInterval()
+  // global variables, event listeners, setInterval(), closure are the common ways to do memory-leak
   Interpreter/Ignition => read the code, run the code(no compilation)
   Bytecode => interpreter produces bytecode (low-level code but not as low-level as machine-code)
   Profiler/Monitor => tells how we can optimize the code
   JIT-Compiler/Turbo-Fan => read the code, understands it, convert the code into another language (machine-code), produces optimized code
   Optimized-Code => mixture of bytecode and machine-code
-  // Memoization => Memoization is a way to cache a return value of a function based on its parameters. This makes the function that takes a long time to run much faster after one execution. If the parameter changes, it will still have to re-evaluate the function.
-  // Memoization
+  // Memoization => code optimization technique, Memoization is a way to cache a return value of a function based on its parameters. This makes the function that takes a long time to run much faster after one execution. If the parameter changes, it will still have to re-evaluate the function.
   // Bad Way
   function addTo80(n) {
-  console.log('long time...')
-  return n + 80
+    console.log('long time...')
+    return n + 80
   }
   addTo80(5)
   addTo80(5)
   addTo80(5)
-  // long time... 85
-  // long time... 85
-  // long time... 85
   // Memoized Way
-  functions memoizedAddTo80() {
-  let cache = {}
-  return function(n) { // closure to access cache obj
-  if (n in cache) {
-  return cache[n]
-  } else {
-  console.log('long time...')
-  cache[n] = n + 80
-  return cache[n]
-  }}}
+  function memoizedAddTo80() {
+    let cache = {};
+    return function (n) {
+      // closure to access cache obj
+      if (n in cache) {
+        return cache[n];
+      } else {
+        console.log("long time...");
+        cache[n] = n + 80;
+        return cache[n];
+      }
+    };
+  }
   const memoized = memoizedAddTo80()
   console.log('1.', memoized(5))
   console.log('2.', memoized(5))
-  console.log('3.', memoized(5))
   console.log('4.', memoized(10))
-  // long time...
-  // 1. 85
-  // 2. 85
-  // 3. 85
-  // long time...
-  // 4. 90
 
-  // don't use these things in your code [eval(), arguments, for-in loop, with, delete, hidden-classes, inline-caching]
+  // Don't use these things in your code [eval(), arguments, for-in loop, with, delete, hidden-classes, inline-caching]
   // Hidden-Classes
   // Order should be same for properties OR assign properties of an object in its Constructor
   function Animal(x,y){
@@ -100,7 +94,7 @@ JS-Engine
 
 // Single-Threaded => only 1 set of instruction is executed at a time (1 call-stack means single-threaded, Synchronous)
 // Javascript Runtime => for Asynchronous behaviour [Call-Stack, Memory-Heap, Web-API[DOM, fetch(), setTimeout()], Event-Loop, Job-Queue, Callback-Queue, Worker-Threads]
-// Node.js => Javascript Runtime 
+// Node.js => Javascript Runtime
 // Babel => Javascript compiler that takes your modern JS code and returns browser compatible JS.
 // ECMAScript => is the standardized specification that defines how the JavaScript programming language should work. It ensures that JavaScript behaves the same way across all browsers and environments.
 // WebAssembly => standard binary executable format
