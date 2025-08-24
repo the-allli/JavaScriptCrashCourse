248e38f86eb3a38d8025a3945d43f92985f605e5
Ali <ha5403905@gmail.com>
2025-08-24 12:35:58 +0500

    some changes

--- 7_Functions/FCC/script.js
+++ 7_Functions/FCC/script.js
@@ -1,5 +1,20 @@
-// First-Class Functions
-function executeFunction(fn) {
-    fn();
+// Assign a function to a variable
+const greet = function () {
+  console.log("Hello!");
+};
+greet(); // "Hello!"
+
+// Pass a function as an argument
+function sayHello(callback) {
+  callback();
 }
-executeFunction(() => console.log("Executed!"));
+sayHello(greet); // "Hello!"
+
+// Return a function from another function
+function createMultiplier(factor) {
+  return function (number) {
+    return number * factor;
+  };
+}
+const multiplyByFive = createMultiplier(5);
+console.log(multiplyByFive(10)); // 50
